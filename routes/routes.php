0bf1d4ed16bc573d0d7530804d0c4fe3f7b6b20d
24514675 <102307827+24514675@users.noreply.github.com>
2023-05-04 17:21:37 +0100

    Delete routes directory

--- routes/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-	This shows the routes in which i have produced to get the feedback and pull the information or create. 
-*/
-Route::get('/', Data()
-{
-	return View::make('questionnaire');
-});
