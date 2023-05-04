db92290b36895b05924266419bf33a6032e10357
24514675 <102307827+24514675@users.noreply.github.com>
2023-05-04 17:20:12 +0100

    Delete app directory

--- app/compile.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	class <Database class="php"></Database>;
-	/*
-	This shows the different classes which are wished to be included in the process. 
-	*/
-);
--- app/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	/*
-	This sets "database" as the default session libray for the users to be collected from. 
-	*/
-	'Database' => 'array',
-);
